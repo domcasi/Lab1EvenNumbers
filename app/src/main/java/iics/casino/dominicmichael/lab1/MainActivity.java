ab695e6e4682d9ade7494e5db7e008c8ce541160
domcasi <42842878+domcasi@users.noreply.github.com>
2018-08-31 00:50:06 +0800

    no more higner number
    removed higher number. to be moved to own project

--- app/src/main/java/iics/casino/dominicmichael/lab1/MainActivity.java
+++ app/src/main/java/iics/casino/dominicmichael/lab1/MainActivity.java
@@ -34,28 +34,5 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        Button HigherNumGo =(Button) findViewById(R.id.HigherNumGoButton);
-
-        HigherNumGo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView HigherNum = (TextView) findViewById(R.id.HigherNumTextView);
-                EditText HigherNumN = (EditText) findViewById(R.id.HighNumN);
-                EditText HigherNumM = (EditText) findViewById(R.id.HighNumM);
-                int n = Integer.parseInt(HigherNumN.getText().toString());
-                int m = Integer.parseInt(HigherNumM.getText().toString());
-                if(n>m)
-                    HigherNum.setText(n + " is higher than " + m);
-                else if(n<m)
-                    HigherNum.setText(m + " is higher than " + n);
-                else
-                    HigherNum.setText(n + " and " + m + " are the same the number.");
-
-            }
-
-
-
-        });
-
     }
 }
